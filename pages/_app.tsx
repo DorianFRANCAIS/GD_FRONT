fedd41d977a1809f51f692ec6d080b61efa08080
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-11-10 11:40:24 +0100

    add mantine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light"
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   )
 }
