d33ce1aa4afe5837c24bbbf26cdcc363e17abc65
Yann Le Coz <ylcoz@icloud.com>
2022-11-25 13:53:03 +0100

    Set list of Employees

--- container/Dashboard/MyEmployees/MyEmployees.tsx
+++ container/Dashboard/MyEmployees/MyEmployees.tsx
@@ -1,15 +1,130 @@
-import React from 'react'
-import useMyEmployeesStyle from './MyEmployees.style'
+import React, { useEffect, useState } from "react";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  ScrollArea,
+  Table,
+  Text,
+} from "@mantine/core";
+import { IconEye, IconPencil, IconTrash, IconUserPlus } from "@tabler/icons";
 
-const MyEmployees = () => {
-
-    const { classes } = useMyEmployeesStyle();
-
-  return (
-    <div>
-        {/* Make your page here */}
-    </div>
-  )
+interface IEmployee {
+  name: string;
+  firstname: string;
+  email: string;
+  phoneNumber: string;
+  lastConnection: Date;
 }
 
-export default MyEmployees
+const MyEmployees = () => {
+  const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+  useEffect(() => {
+    setEmployees([
+      {
+        name: "Le Coz",
+        firstname: "Yann",
+        email: "ylcoz@icloud.com",
+        phoneNumber: "0645329078",
+        lastConnection: new Date("2022-11-20T11:10:00.000"),
+      },
+      {
+        name: "Chambaud",
+        firstname: "Mathieu",
+        email: "mchambaud2000@gmail.com",
+        phoneNumber: "0645329079",
+        lastConnection: new Date("2021-11-22T11:10:00.000"),
+      },
+      {
+        name: "Français",
+        firstname: "Dorian",
+        email: "dorian.francais@ynov.com",
+        phoneNumber: "0645329060",
+        lastConnection: new Date("2022-10-24T11:10:00.000"),
+      },
+    ]);
+  }, []);
+
+  const rows = employees.map((employee) => (
+    <tr key={employee.name}>
+      <td>
+        <Group spacing="sm">
+          <div>
+            <Text size="sm" weight={500}>
+              {employee.name.toUpperCase()} {employee.firstname}
+            </Text>
+            <Text size="xs" color="dimmed">
+              {employee.email}
+            </Text>
+          </div>
+        </Group>
+      </td>
+
+      <td>{employee.phoneNumber}</td>
+
+      <td>
+        <Group spacing="sm">
+          <div>
+            <Text size="sm" weight={400}>
+              Il y a{" "}
+              {Math.floor(
+                (new Date().getTime() - employee.lastConnection.getTime()) /
+                  8.64e7,
+              )}{" "}
+              jours
+            </Text>
+            <Text size="xs" color="dimmed">
+              le {employee.lastConnection.toLocaleDateString()}
+            </Text>
+          </div>
+        </Group>
+      </td>
+
+      <td>
+        <Group spacing={0} position="center">
+          <ActionIcon onClick={() => console.log("See")}>
+            <IconEye size={16} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon onClick={() => console.log("Edit")}>
+            <IconPencil size={16} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon color="red" onClick={() => console.log("Delete")}>
+            <IconTrash size={16} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <ScrollArea>
+      <Button
+        type="submit"
+        color="green.7"
+        radius="md"
+        mb="xl"
+        component="a"
+        href="/educator/registration"
+      >
+        <ActionIcon mr="xs">
+          <IconUserPlus size={20} stroke={1.5} color="white" />
+        </ActionIcon>
+        Ajouter un nouvel employé
+      </Button>
+      <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
+        <thead>
+          <tr>
+            <th>Employés</th>
+            <th>Numéro de téléphone</th>
+            <th>Dernière connexion</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
+  );
+};
+
+export default MyEmployees;
