56511ee607c1d534b15f7573c1e36c79ce3f5a9f
M@thieu Ch@mb@ud <43409975+FrekiManagarm@users.noreply.github.com>
2022-12-01 17:19:51 +0100

    Feature/helpers (#15)
    * add helpers
    
    * fix recup user
    
    * fix error on CI
    
    * push middleware
    
    * fix middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import Layout from "../container/Layout/Layout";
 import { DefaultSeo } from "next-seo";
 import AuthProvider from "../context/AuthProvider";
+import { withData } from "../helpers/restrictions";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -35,4 +36,29 @@ export default function App({ Component, pageProps }: AppProps) {
       </MantineProvider>
     </AuthProvider>
   );
+}
+
+App.getInitialProps = async ({ Component, ctx }: { Component: any, ctx : any }) => {
+  let pageProps = {}
+  const { query, pathname, asPath, res } = ctx
+  const { user, isLoggedIn, token } = await withData(ctx);
+  const tokenCookie = token;
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  let isProtectedRoute = [
+    `/dashboard/[[...tab]]`,
+    // `/configurator`
+  ].includes(pathname);
+
+  if (!isLoggedIn && isProtectedRoute && res) {
+    res.writeHead(302, {
+      Location: `${process.env.LOCAL_FRONT_SERVER}/login?next=${asPath}`,
+    });
+    res.end();
+  }
+
+  return { pageProps, query, pathname, user, isLoggedIn, tokenCookie };
 }
