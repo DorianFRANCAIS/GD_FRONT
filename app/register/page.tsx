df507dd736908094dc07bdde1d61d7a31e50e8c5
CorentinGarnier <corentin.garnier40@gmail.com>
2023-05-12 14:34:49 +0200

    register & login form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,15 +3,16 @@
 import {SubmitHandler, useForm} from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import axios from "axios";
+import { axiosClient } from "@/utils/apiClient";
 
 
 const registerSchema = yup.object({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    emailAddress: yup.string().required().email(),
-    birthDate: yup.string().required(),
-    password: yup.string().required(),
+    firstname: yup.string().required('Veuillez renseigner votre Nom'),
+    lastname: yup.string().required('Veuillez renseigner votre Prénom'),
+    emailAddress: yup.string().required('Veuillez renseigner un E-mail').email(),
+    birthDate: yup.string().required('Veuillez renseigner votre date de naissance'),
+    phoneNumber: yup.string().required('Veuillez renseigner votre numéro de téléphone'),
+    password: yup.string().required('Mot de passe invalide'),
     passwordConfirm:yup.string()
         .required('Veuillez saisir de nouveau votre mot de passe')
         .oneOf(
@@ -28,11 +29,17 @@ export default function RegisterPage() {
     });
 
     const onSubmit: SubmitHandler<FormData> = async (
-        data: FormData,
+        data: FormData
+        ,
     ) => {
         try {
-            const response = await axios.post('https://dev.api.gestidogs.ianlcz.io/users/register',data)
-            console.log(response)
+            const response = await axiosClient.post('https://dev.api.gestidogs.ianlcz.io/users/register',data,{
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Accept': 'application/json',
+                      'Access-Control-Allow-Origin': '*'
+                  }
+            })
             return response
         } catch (error) {
             throw error;
@@ -45,22 +52,38 @@ export default function RegisterPage() {
             <form className="mt-12 flex w-full flex-col items-stretch justify-center" onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col gap-3">
                     <div className="sm:flex gap-3">
-                        <input className="my-2" type="text" {...register("lastName")} placeholder="Votre nom"/>
-                        <p>{errors.lastName?.message}</p>
-                        <input className="my-2" type="text" {...register("firstName")}   placeholder="Votre prénom"/>
-                        <p>{errors.firstName?.message}</p>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="text" {...register("lastname")} placeholder="Votre nom"/>
+                            <p className="text-xs text-red-600">{errors.lastname?.message}</p>
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="text" {...register("firstname")}   placeholder="Votre prénom"/>
+                            <p className="text-xs text-red-600">{errors.firstname?.message}</p>
+                        </div>
                     </div>
                     <div className="sm:flex gap-3">
-                        <input className="my-2" type="email" {...register("emailAddress")} placeholder="E-mail"/>
-                        <p>{errors.emailAddress?.message}</p>
-                        <input className="my-2" type="date" {...register("birthDate")} placeholder="Date de naissance"/>
-                        <p>{errors.birthDate?.message}</p>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="email" {...register("emailAddress")} placeholder="E-mail"/>
+                            <p className="text-xs text-red-600">{errors.emailAddress?.message}</p>
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="date" {...register("birthDate")} placeholder="Date de naissance"/>
+                            <p className="text-xs text-red-600">{errors.birthDate?.message}</p>
+                        </div>
+                    </div>
+                    <div className="sm:flex flex-col">
+                        <input className="my-2" type="phone" {...register("phoneNumber")} placeholder="Téléphone"/>
+                        <p className="text-xs text-red-600">{errors.phoneNumber?.message}</p>
                     </div>
                     <div className="sm:flex gap-3">
-                        <input className="my-2" type="password" {...register("password")} placeholder="Mot de passe"/>
-                        <p>{errors.password?.message}</p>
-                        <input className="my-2" type="password" {...register("passwordConfirm")} placeholder="Confirmez votre mot de passe"/>
-                        <p>{errors.passwordConfirm?.message}</p>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="password" {...register("password")} placeholder="Mot de passe"/>
+                            <p className="text-xs text-red-600">{errors.password?.message}</p>
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <input className="my-2" type="password" {...register("passwordConfirm")} placeholder="Confirmez votre mot de passe"/>
+                            <p className="text-xs text-red-600">{errors.passwordConfirm?.message}</p>
+                        </div>
                     </div>
                 </div>
                 <button className="btn mt-5" type="submit" >Enregistrer</button>
---
+++ utils/apiClient.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_BACK_OPX_API_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    // Si la réponse contient une exception, on affiche un message d'erreur dans un toast
+    if (response.data.exception) {
+      /*toast.error(
+        `${
+          response.data.exception && response.data.exception !== ''
+            ? `${response.data.exception}`
+            : ''
+        } \n\n${response.data.message}`
+      );*/
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      // La requête a été faite mais le serveur a répondu avec une erreur
+      const { data, status } = error.response;
+      if (
+        data.errors !== undefined &&
+        data.errors.includes('Unauthenticated.')
+      ) {
+        localStorage.removeItem('token');
+        window.location.href = '/connexion';
+        /*toast.error(() => {
+          return `Vous n'êtes pas autorisé à accéder à la plateforme.`;
+        });*/
+      }
+      console.log('Request made but the server responded with an error :', {
+        data,
+        status,
+      });
+    } else if (error.request) {
+      // La requête a été faite mais aucune réponse n'a été reçue du serveur
+      console.log(
+        'Request made but no response is received from the server :',
+        error.request
+      );
+    } else {
+      // Une erreur s'est produite lors du setup de la requête
+      console.log(
+        'Error occured while setting up the request :',
+        error.message
+      );
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { axiosClient };
