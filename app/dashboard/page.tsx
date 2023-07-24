d933b325c2e04d971de7a04fa8b79a1e566fa61b
GARNIER Corentin <cgarnier@incomm.fr>
2023-07-24 16:06:45 +0200

    add activities on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,6 +10,9 @@ import { GetAllStaff } from "@/pages/api/users/getUserInformations";
 import { IUser } from "@/types/IUser";
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
+import Link from "next/link";
+import { IActivity } from "@/types/IActivity";
+import { getActivities } from "@/pages/api/activities/activitiesApi";
 
 
 
@@ -19,10 +22,12 @@ async function Dashboard() {
     let dogs: IDogs[] = [];
     let sessions: IDailySession | null = null;
     let usersStaff: IUser[] = [];
+    let activities: IActivity[] = [];
     if (establishments.length > 0) {
         dogs = await handleDogs(session, establishments[0]._id);
         sessions = await GetDailySessions(session, establishments[0]._id, format(new Date(), 'yyyy-MM-dd'));
         usersStaff = await GetAllStaff(session, establishments[0]._id);
+        activities = await getActivities(session, establishments[0]._id);
     }
 
     return (
@@ -58,7 +63,10 @@ async function Dashboard() {
             </div>
             <div className="flex flex-col gap-y-8">
                 <div className="wrapper">
-                    <h3 className="text-mainColor text-2xl font-bold">Mes chiens</h3>
+                    <div className="flex justify-between items-center">
+                        <h3 className="text-mainColor text-2xl font-bold">Mes chiens</h3>
+                        <Link href="/dogs" className="text-mainColor cursor-pointer">Voir plus</Link>
+                    </div>
                     <div className="flex flex-grow items-center gap-x-2">
                         {dogs && dogs.map((dog, idx) => (
                             <div key={idx} className="flex flex-col justify-center items-center">
@@ -73,7 +81,10 @@ async function Dashboard() {
                     </div>
                 </div>
                 <div className="wrapper">
-                    <h3 className="text-mainColor text-2xl font-bold">Mon équipe</h3>
+                    <div className="flex justify-between items-center">
+                        <h3 className="text-mainColor text-2xl font-bold">Mon équipe</h3>
+                        <Link href="/team" className="text-mainColor cursor-pointer">Voir plus</Link>
+                    </div>
                     <div className="flex flex-grow items-center gap-x-2">
                         {usersStaff && usersStaff.map((staff, idx) => (
                             <div key={idx} className="flex flex-col justify-center items-center">
@@ -89,42 +100,32 @@ async function Dashboard() {
                     </div>
                 </div>
                 <div className="wrapper">
-                    <h3 className="text-mainColor text-2xl font-bold">Mes activités</h3>
-                    <div className="flex flex-col gap-y-12 mt-9">
-                        <div className="flex justify-between bg-greyColor rounded-twenty px-5 py-2" >
-                            <img
-                                src="/img/avatar.svg"
-                                alt="Profile"
-                                className="avatar rounded-full"
-                            />
-                            <div className="flex flex-col font-ws font-bold">
-                                <p>Type : Checking annuel</p>
-                                <p>ce Lundi 12 juillet 2023 à 14H30</p>
-                                <p>Tarif : 120€</p>
+                    <div className="flex justify-between items-center">
+                        <h3 className="text-mainColor text-2xl font-bold">Mes activités</h3>
+                        <Link href="/activities" className="text-mainColor cursor-pointer">Voir plus</Link>
+                    </div>
+                    <div className="flex flex-col gap-y-6 mt-9">
+                        {activities && activities.map((activity, idx) => (
+                            <div key={idx} className="flex w-full bg-greyColor p-2 rounded-twenty" >
+                                <div className="flex items-center w-full">
+                                    <div className="w-2/6">
+                                        <img
+                                            src={activity.imageUrl}
+                                            alt="Profile"
+                                            className="h-32 w-full rounded-twenty"
+                                        />
+                                    </div>
+
+                                    <div className="w-4/6 h-full flex flex-col px-2">
+                                        <h3 className="text-center text-mainColor font-bold">{activity.title}</h3>
+                                        <p className="text-xs"><span className="font-bold">Description :</span> {activity.description}</p>
+                                        <p className="text-xs"><span className="font-bold">Durée :</span> {activity.duration}min</p>
+                                        <p className="text-xs"><span className="font-bold">Prix :</span> {activity.price}€</p>
+                                    </div>
+                                </div>
                             </div>
-                            <img
-                                src="/img/avatar.svg"
-                                alt="Profile"
-                                className="avatar rounded-full"
-                            />
-                        </div>
-                        <div className="flex justify-between bg-greyColor rounded-twenty px-5 py-2">
-                            <img
-                                src="/img/avatar.svg"
-                                alt="Profile"
-                                className="avatar rounded-full"
-                            />
-                            <div className="flex flex-col font-ws font-bold">
-                                <p>Type : Checking annuel</p>
-                                <p>ce Lundi 12 juillet 2023 à 14H30</p>
-                                <p>Tarif : 120€</p>
-                            </div>
-                            <img
-                                src="/img/avatar.svg"
-                                alt="Profile"
-                                className="avatar rounded-full"
-                            />
-                        </div>
+                        ))}
+
                     </div>
                 </div>
             </div>
